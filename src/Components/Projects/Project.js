1c743fea3b76a61ab6c4ac8236d8c373a30f268b
Ana Sander <anagsander@gmail.com>
2023-04-03 16:33:47 -0300

    cat animation title

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -1,25 +1,45 @@
 import React from "react";
 import styles from "./Project.module.css";
 
-const Project = ({ project: { title, date, img, misc } }) => {
+const Project = ({ project: { title, date, img, misc, background } }) => {
   return (
     <div className={styles.project}>
-      <div className={styles.projectBg}>
-        <div>
-          <h1 className="size32">{title}</h1>
-          <p className="size24">{date}</p>
-          <img src={img} alt="" />
-          <div className={styles.projectFlex}>
-            {misc.map((i) => (
-              <p className={`size24 ${styles.ability}`} key={i}>
-                {i}
-              </p>
-            ))}
-          </div>
+      <div className={styles.projectBg} style={{ background: background }}>
+        <h1 className="size32">{title}</h1>
+        <p className="size24">{date}</p>
+        <img src={img} alt="" />
+        <div className={styles.projectFlex}>
+          {misc.map((i) => (
+            <p className={`size24 ${styles.ability}`} key={i}>
+              {i}
+            </p>
+          ))}
         </div>
       </div>
     </div>
   );
 };
+
+// const Project = () => {
+//   return (
+//     <div class={styles.container}>
+//       <div class={styles.card}>
+//         <p>Texto.</p>
+//       </div>
+//       <div class={styles.card}>
+//         <p>Texto.</p>
+//       </div>
+//       <div class={styles.card}>
+//         <p>Texto.</p>
+//       </div>
+//       <div class={styles.card}>
+//         <p>Texto.</p>
+//       </div>
+//       <div class={styles.card}>
+//         <p>Texto.</p>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Project;
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -10,34 +10,33 @@ const Projects = () => {
       date: "01/01/2001",
       img: images.dogsSquare,
       misc: ["HTML", "CSS", "JavaScript", "React"],
+      background: "#000",
     },
     {
       title: "Cats",
       date: "01/01/2001",
       img: images.dogsSquare,
       misc: ["HTML", "CSS", "JavaScript", "React"],
+      background: "#fff",
     },
     {
       title: "LOL",
       date: "01/01/2001",
       img: images.dogsSquare,
       misc: ["HTML", "CSS", "JavaScript", "React"],
+      background: "#e13",
     },
   ];
 
   return (
     <section className={styles.projetoTopo}>
-      <div>
-        <h2 className={`titulo container ${styles.tituloProjetos}`}>
-          Projetos
-        </h2>
-        <div className={styles.projectsBg}>
-          <div className="container">
-            <div className={styles.projects}>
-              {ProjectsList.map((project, index) => (
-                <Project key={index} index={index} project={project} />
-              ))}
-            </div>
+      <h2 className={`titulo container ${styles.tituloProjetos}`}>Projetos</h2>
+      <div className={styles.projectsBg}>
+        <div className="container">
+          <div className={styles.projects}>
+            {ProjectsList.map((project, index) => (
+              <Project key={index} index={index} project={project} />
+            ))}
           </div>
         </div>
       </div>
