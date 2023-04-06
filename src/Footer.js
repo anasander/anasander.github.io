b91330a971f1edb7a982acefe9ebbb86435d3c61
Ana Sander <anagsander@gmail.com>
2023-04-06 15:35:16 -0300

    footer mobile

--- src/Footer.js
+++ src/Footer.js
@@ -9,19 +9,28 @@ const Footer = () => {
       <div className={styles.footer}>
         <div className="container padding">
           <div className={styles.redes}>
-            <a href="mailto: anagsander@gmail.com">
+            <a className={styles.redesLink} href="mailto: anagsander@gmail.com">
               <img src={images.email} alt="E-mail" />
             </a>
-            <a href="https://www.linkedin.com/in/ana-gabriela-sander-morais-ivatio-b2302019b/">
+            <a
+              className={styles.redesLink}
+              href="https://www.linkedin.com/in/ana-gabriela-sander-morais-ivatio-b2302019b/"
+            >
               <img src={images.linkedin} alt="LinkedIn" />
             </a>
-            <a href="https://github.com/anasander">
+            <a className={styles.redesLink} href="https://github.com/anasander">
               <img src={images.github} alt="GitHub" />
             </a>
-            <a href="https://twitter.com/LadySander">
+            <a
+              className={styles.redesLink}
+              href="https://twitter.com/LadySander"
+            >
               <img src={images.twitter} alt="Twitter" />
             </a>
-            <a href="https://www.instagram.com/anasander/">
+            <a
+              className={styles.redesLink}
+              href="https://www.instagram.com/anasander/"
+            >
               <img src={images.instagram} alt="Intagram" />
             </a>
           </div>
